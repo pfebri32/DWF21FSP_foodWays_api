20d3eb7248830263d6c191eb33a04524faf55ab0
pfebri <personal.febri32@gmail.com>
2021-03-18 03:10:18 +0700

    Add Register

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -3,7 +3,7 @@ const { User } = require('../../models');
 
 exports.login = async (req, res) => {
   try {
-    const { body, params } = req;
+    const { body } = req;
     const { email, password } = body;
 
     // Find user with a same email.
@@ -17,10 +17,10 @@ exports.login = async (req, res) => {
     if (user) {
       // Check if an email with a same password.
       if (user.password === password) {
-        // What password is correct.
+        // When password is correct.
         return res.send({
           status: 'success',
-          message: 'Login is successful.',
+          message: 'Login is success.',
           data: {
             user: {
               name: user.fullname,
@@ -30,18 +30,59 @@ exports.login = async (req, res) => {
           },
         });
       } else {
-        // What password is incorrect.
+        // When password is incorrect.
         return res.send({
           status: 'failed',
           message: 'Your password is incorrect.',
         });
       }
     } else {
+      // When user with email doesn't exist.
       return res.send({
         status: 'failed',
         message: "Your email doesn't exist.",
       });
     }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.register = async (req, res) => {
+  try {
+    const { body } = req;
+    const { email } = body;
+
+    // Find user with a same email.
+    const isEmailExist = await User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    // Check if user with a same email is exist and if exist will failed.
+    if (isEmailExist) {
+      return res.send({
+        status: 'failed',
+        message: 'User with this email is already exist.',
+      });
+    }
+
+    // Create new user.
+    const user = await User.create(body);
+    const { fullname, role } = user;
+
+    res.send({
+      status: 'success',
+      message: 'Resgiter is success.',
+      data: {
+        user: {
+          fullname,
+          token: 'token',
+          role,
+        },
+      },
+    });
   } catch (err) {
     console.log(err);
   }
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
-const { login } = require('../controllers/auth');
+const { login, register } = require('../controllers/auth');
 
 router.post('/api/v1/login', login);
+router.post('/api/v1/register', register);
 
 const { getUsers } = require('../controllers/user');
 
