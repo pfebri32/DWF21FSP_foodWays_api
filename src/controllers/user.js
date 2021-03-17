01697d0134b1964df5ecdcfa412aeddead5b065e
pfebri <personal.febri32@gmail.com>
2021-03-18 03:23:21 +0700

    Add Users and Delete User.

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -9,3 +9,23 @@ exports.getUsers = async (req, res) => {
     },
   });
 };
+
+exports.deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await User.destroy({
+      where: {
+        id,
+      },
+    });
+    res.send({
+      status: 'success',
+      message: `User with ID = ${id} has been deleted.`,
+      data: {
+        id,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -7,8 +7,9 @@ const { login, register } = require('../controllers/auth');
 router.post('/api/v1/login', login);
 router.post('/api/v1/register', register);
 
-const { getUsers } = require('../controllers/user');
+const { getUsers, deleteUser } = require('../controllers/user');
 
 router.get('/api/v1/users', getUsers);
+router.delete('/api/v1/user/:id', deleteUser);
 
 module.exports = router;
